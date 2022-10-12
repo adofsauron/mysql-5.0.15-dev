cf91dc13640bad80f4619d306da718255aa12f04
adofsauron <adofsauron@163.com>
2022-10-12 19:19:50 +0800

    fix mysql 5.0.15

--- trunk/format.sh
+++ trunk/format.sh
@@ -3,8 +3,7 @@
 
 cd mysql-5.0.15
 
-clang-format -style=file -i   ./sql/sql_select.h
-clang-format -style=file -i   ./sql/sql_select.cc
+clang-format -style=file -i   ./sql/*.h
+clang-format -style=file -i   ./sql/*.cc
 
 cd -
-

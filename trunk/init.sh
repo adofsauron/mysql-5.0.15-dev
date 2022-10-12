a086a32f307143c1d0eca803358ef639a5cd24e3
adofsauron <adofsauron@163.com>
2022-10-12 18:32:40 +0800

    fix mysql 5.0.15

--- trunk/init.sh
+++ trunk/init.sh
@@ -6,6 +6,5 @@ mkdir -p /usr/local/share/mysql/data
 
 chown -R mysql:mysql /usr/local/share/mysql/data 
 
-mysql_install_db --user=mysql --datadir=/usr/local/share/mysql/data
-
+/usr/local/bin/mysql_install_db --user=mysql --datadir=/usr/local/share/mysql/data
 

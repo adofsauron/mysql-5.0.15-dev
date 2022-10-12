25a68fabfafaef9b852d53a1aef133e76ef0615c
adofsauron <adofsauron@163.com>
2022-10-12 15:05:08 +0800

    fix mysql 5.0.15

--- trunk/mysql-5.0.15/mysql_start.sh
+++ trunk/mysql-5.0.15/mysql_start.sh
@@ -4,6 +4,6 @@ pkill mysqld
 
 sleep 1s
 
-mysqld --defaults-file=./my.cnf --user=mysql --datadir=/usr/local/share/mysql/data &
+/usr/local/libexec/mysqld --defaults-file=./my.cnf --user=mysql --datadir=/usr/local/share/mysql/data &
 
 

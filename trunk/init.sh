06d3de0807e13ef59f23c4a55949ce304a752bf7
CodingSuen <69141572+CodingSuen@users.noreply.github.com>
2022-10-26 15:23:20 +0800

    Update init.sh

--- trunk/init.sh
+++ trunk/init.sh
@@ -4,6 +4,10 @@ rm /usr/local/share/mysql/data -rf
 
 mkdir -p /usr/local/share/mysql/data 
 
+groupadd -g 1001 mysql 
+useradd -u 1001 -g mysql mysql 
+echo 'mysql' | passwd --stdin mysql
+
 chown -R mysql:mysql /usr/local/share/mysql/data 
 
 /usr/local/bin/mysql_install_db --user=mysql --datadir=/usr/local/share/mysql/data

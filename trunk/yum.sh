708770439af82ade2168006e8b211fb8ff2e3d19
zhangshilong <adofsauron@163.com>
2024-05-06 14:12:54 +0800

    fix mysql 5.0.15

--- trunk/yum.sh
+++ trunk/yum.sh
@@ -3,5 +3,5 @@
 yum install -y epel-release
 yum install -y ncurses-devel
 yum install -y gcc gcc-c++ gdb make cmake
-yum install -y dos2unix clang vim lsof
+yum install -y dos2unix clang vim lsof perf
 
